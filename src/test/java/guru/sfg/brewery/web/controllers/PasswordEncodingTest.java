4d28183a872e83138940b6292516c73c35f06fdd
Mateusz Ziebura <mateusz_860@op.pl>
2021-06-29 10:22:11 +0200

    LDAP Password Encoder

--- src/test/java/guru/sfg/brewery/web/controllers/PasswordEncodingTest.java
+++ src/test/java/guru/sfg/brewery/web/controllers/PasswordEncodingTest.java
@@ -1,15 +1,30 @@
 package guru.sfg.brewery.web.controllers;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.security.crypto.password.LdapShaPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.util.DigestUtils;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class PasswordEncodingTest {
 
     static final String PASSWORD = "password";
 
+
+    @Test
+    void Ldap() {
+        PasswordEncoder ldap = new LdapShaPasswordEncoder();
+
+        System.out.println(ldap.encode(PASSWORD));
+        System.out.println(ldap.encode(PASSWORD));
+
+        String result = ldap.encode(PASSWORD);
+
+        assertTrue(ldap.matches(PASSWORD,result));
+    }
     @Test
     void testNoOp(){
         PasswordEncoder noOP = NoOpPasswordEncoder.getInstance();
