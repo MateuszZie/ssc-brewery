e3de6180ba49fc3fd457a6bf740181c403162849
Mateusz Ziebura <mateusz_860@op.pl>
2021-07-01 16:00:59 +0200

    Configuring User Roles

---
+++ src/test/java/guru/sfg/brewery/web/controllers/BreweryControllerTest.java
@@ -0,0 +1,16 @@
+package guru.sfg.brewery.web.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BreweryControllerTest {
+
+    @Test
+    void listBreweries() {
+    }
+
+    @Test
+    void getBreweriesJson() {
+    }
+}
--- src/test/java/guru/sfg/brewery/web/controllers/api/BeerRestControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/api/BeerRestControllerIT.java
@@ -14,6 +14,33 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 public class BeerRestControllerIT extends BaseIT {
 
     @Test
+    void deleteBeerHttpBasic() throws Exception{
+        mockMvc.perform(delete("/api/v1/beer/97df0c39-90c4-4ae0-b663-453e8e19c311")
+                .with(httpBasic("spring", "mateusz")))
+                .andExpect(status().is2xxSuccessful());
+    }
+
+    @Test
+    void deleteBeerHttpBasicUserRole() throws Exception{
+        mockMvc.perform(delete("/api/v1/beer/97df0c39-90c4-4ae0-b663-453e8e19c311")
+                .with(httpBasic("user", "password")))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    void deleteBeerHttpBasicCustomerRole() throws Exception{
+        mockMvc.perform(delete("/api/v1/beer/97df0c39-90c4-4ae0-b663-453e8e19c311")
+                .with(httpBasic("scott", "tiger")))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    void deleteBeerNoAuth() throws Exception{
+        mockMvc.perform(delete("/api/v1/beer/97df0c39-90c4-4ae0-b663-453e8e19c311"))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
     void deleteBeerUrl() throws Exception{
         mockMvc.perform(delete("/api/v1/beer/97df0c39-90c4-4ae0-b663-453e8e19c311")
                 .param("apiKey","spring").param("apiSecret", "mateusz"))
@@ -37,21 +64,8 @@ public class BeerRestControllerIT extends BaseIT {
     @Test
     void deleteBeer() throws Exception {
         mockMvc.perform(delete("/api/v1/beer/97df0c39-90c4-4ae0-b663-453e8e19c311")
-        .header("Api-Key","spring").header("Api-Secret","mateusz"))
+                .header("Api-Key", "spring").header("Api-Secret", "mateusz"))
                 .andExpect(status().isOk());
-
-    }
-    @Test
-    void deleteBeerHttpBasic() throws Exception{
-        mockMvc.perform(delete("/api/v1/beer/97df0c39-90c4-4ae0-b663-453e8e19c311")
-                .with(httpBasic("spring", "mateusz")))
-                .andExpect(status().is2xxSuccessful());
-    }
-
-    @Test
-    void deleteBeerNoAuth() throws Exception{
-        mockMvc.perform(delete("/api/v1/beer/97df0c39-90c4-4ae0-b663-453e8e19c311"))
-                .andExpect(status().isUnauthorized());
     }
 
     @Test
