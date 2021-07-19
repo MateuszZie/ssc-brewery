9ace95928afa978c2dae87854c0e2db139791ae3
Mateusz Ziebura <mateusz_860@op.pl>
2021-07-19 16:44:44 +0200

    Configure User Entity for 2FA

--- src/main/java/guru/sfg/brewery/domain/security/User.java
+++ src/main/java/guru/sfg/brewery/domain/security/User.java
@@ -63,6 +63,14 @@ public class User implements UserDetails, CredentialsContainer {
     @Builder.Default
     private  boolean enabled =true;
 
+    @Builder.Default
+    private boolean userGoogle2fa = false;
+
+    private String google2faSecret;
+
+    @Transient
+    private boolean google2faRequired = true;
+
     @Override
     public void eraseCredentials() {
         this.password = null;
