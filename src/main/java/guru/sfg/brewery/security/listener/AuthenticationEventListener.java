3e92404d2e06288aff7dbe0964609d1269b31294
Mateusz Ziebura <mateusz_860@op.pl>
2021-07-19 10:48:12 +0200

    Persistence of Authentication Success Events

--- src/main/java/guru/sfg/brewery/security/listener/AuthenticationEventListener.java
+++ src/main/java/guru/sfg/brewery/security/listener/AuthenticationEventListener.java
@@ -1,16 +1,24 @@
 package guru.sfg.brewery.security.listener;
 
+import guru.sfg.brewery.domain.security.LoginSuccess;
 import guru.sfg.brewery.domain.security.User;
+import guru.sfg.brewery.repositories.security.LoginSuccessRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.event.AuthenticationFailureBadCredentialsEvent;
 import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
 import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class AuthenticationEventListener {
+
+    private final LoginSuccessRepository successRepository;
 
     @EventListener
     public void listen(AuthenticationSuccessEvent event){
@@ -20,10 +28,34 @@ public class AuthenticationEventListener {
         if (event.getSource() instanceof UsernamePasswordAuthenticationToken) {
             UsernamePasswordAuthenticationToken token = (UsernamePasswordAuthenticationToken) event.getSource();
 
+            LoginSuccess builder = LoginSuccess.builder().build();
+
             if(token.getPrincipal() instanceof User){
                 User user = (User) token.getPrincipal();
-
+                builder.setUser(user);
                 log.debug("User name logged in: " + user.getUsername() );
+            }
+
+            if(token.getDetails() instanceof WebAuthenticationDetails){
+                WebAuthenticationDetails details = (WebAuthenticationDetails) token.getDetails();
+                builder.setSourceIp(details.getRemoteAddress());
+                log.debug("Source IP: " + details.getRemoteAddress());
+            }
+            LoginSuccess loginSuccess = successRepository.save(builder);
+            log.debug("Login success id: " + loginSuccess.getId());
+        }
+
+    }
+    @EventListener
+    public void listenFail(AuthenticationFailureBadCredentialsEvent event){
+
+        log.debug("User Logg in Fail");
+
+        if (event.getSource() instanceof UsernamePasswordAuthenticationToken) {
+            UsernamePasswordAuthenticationToken token = (UsernamePasswordAuthenticationToken) event.getSource();
+
+            if (token.getPrincipal() instanceof String){
+                log.debug("Failed User name: " + token.getPrincipal());
             }
 
             if(token.getDetails() instanceof WebAuthenticationDetails){
@@ -33,4 +65,5 @@ public class AuthenticationEventListener {
             }
         }
     }
+
 }
