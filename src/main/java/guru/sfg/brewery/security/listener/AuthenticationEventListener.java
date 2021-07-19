3bb5bd3dbdfb1209ece7c634bf03f3f38ebce713
Mateusz Ziebura <mateusz_860@op.pl>
2021-07-19 12:14:59 +0200

    Persistence ofAuthentication Failure Events Locking User Account After Failed Attempts

--- src/main/java/guru/sfg/brewery/security/listener/AuthenticationEventListener.java
+++ src/main/java/guru/sfg/brewery/security/listener/AuthenticationEventListener.java
@@ -1,17 +1,23 @@
 package guru.sfg.brewery.security.listener;
 
+import guru.sfg.brewery.domain.security.LoginFailure;
 import guru.sfg.brewery.domain.security.LoginSuccess;
 import guru.sfg.brewery.domain.security.User;
+import guru.sfg.brewery.repositories.security.LoginFailedRepository;
 import guru.sfg.brewery.repositories.security.LoginSuccessRepository;
+import guru.sfg.brewery.repositories.security.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.authentication.event.AuthenticationFailureBadCredentialsEvent;
 import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
 import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Component;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Slf4j
 @Component
@@ -19,6 +25,8 @@ import org.springframework.stereotype.Component;
 public class AuthenticationEventListener {
 
     private final LoginSuccessRepository successRepository;
+    private final LoginFailedRepository failedRepository;
+    private final UserRepository userRepository;
 
     @EventListener
     public void listen(AuthenticationSuccessEvent event){
@@ -53,17 +61,39 @@ public class AuthenticationEventListener {
 
         if (event.getSource() instanceof UsernamePasswordAuthenticationToken) {
             UsernamePasswordAuthenticationToken token = (UsernamePasswordAuthenticationToken) event.getSource();
-
+            LoginFailure.LoginFailureBuilder builder = LoginFailure.builder();
             if (token.getPrincipal() instanceof String){
                 log.debug("Failed User name: " + token.getPrincipal());
+                if(userRepository.findByUsername( (String) token.getPrincipal()).isPresent()){
+                   builder.user(userRepository.findByUsername( (String) token.getPrincipal()).get());
+                   log.debug("User saved");
+                }
             }
 
             if(token.getDetails() instanceof WebAuthenticationDetails){
                 WebAuthenticationDetails details = (WebAuthenticationDetails) token.getDetails();
 
                 log.debug("Source IP: " + details.getRemoteAddress());
+                builder.sourceIp(details.getRemoteAddress());
+            }
+            LoginFailure failed = failedRepository.save(builder.build());
+            log.debug("Failed login id: " +failed.getId());
+
+            if(failed.getUser()!=null){
+                lockUserAccount(failed.getUser());
             }
         }
+    }
+
+    private void lockUserAccount(User user) {
+        List<LoginFailure> loginFailures = failedRepository.findAllByUserAndCreatedDateIsAfter(user,
+                Timestamp.valueOf(LocalDateTime.now().minusDays(1)));
+        if(loginFailures.size()>3){
+            log.debug("Account locked");
+            user.setAccountNonLocked(false);
+            userRepository.save(user);
+        }
+
     }
 
 }
