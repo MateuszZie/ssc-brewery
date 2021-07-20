bd3eaa29ed43fbdb6e436eafd78531a439e931e7
Mateusz Ziebura <mateusz_860@op.pl>
2021-07-20 08:39:23 +0200

    2FA Verification UI

--- src/main/java/guru/sfg/brewery/web/controllers/UserController.java
+++ src/main/java/guru/sfg/brewery/web/controllers/UserController.java
@@ -9,10 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @Controller
@@ -46,6 +43,21 @@ public class UserController {
             return "index";
         }else return "user/register2fa";
 
+    }
+    @GetMapping("/verify2fa")
+    public String verify2fa(){
+        return "user/verify2fa";
+    }
+
+    @PostMapping
+    public String verifyPostOf2Fa(@RequestParam Integer verifyCode){
+        User user = getUser();
+
+        if(googleAuthenticator.authorizeUser(user.getUsername(), verifyCode)){
+            ((User) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).setGoogle2faRequired(false);
+            return "index";
+        }
+        return "user/verify2fa";
     }
 
     private User getUser() {
